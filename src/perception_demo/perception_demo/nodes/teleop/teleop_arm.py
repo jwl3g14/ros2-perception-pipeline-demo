c8b6c7bf2a8fc2ecf6ab3233547de76f6baa23c4
Jin Wei Lim <jinweilim@Jins-MacBook-Pro.local>
2026-02-10 01:04:49 +0800

    Swap teleop directions: X/Z for tilt, S/A for bend

--- src/perception_demo/perception_demo/nodes/teleop/teleop_arm.py
+++ src/perception_demo/perception_demo/nodes/teleop/teleop_arm.py
@@ -4,8 +4,8 @@ Keyboard teleoperation for robot arm.
 
 Key mappings:
   Q/W - shoulder_pan (rotate base left/right)
-  Z/X - shoulder_lift (tilt up/down)
-  A/S - elbow (bend)
+  X/Z - shoulder_lift (tilt up/down)
+  S/A - elbow (bend)
   1/2 - wrist (rotate)
 
   SPACE - reset to home position
@@ -56,10 +56,10 @@ class TeleopArm(Node):
         self.key_map = {
             'q': (0, 1),   # shoulder_pan +
             'w': (0, -1),  # shoulder_pan -
-            'z': (1, 1),   # shoulder_lift +
-            'x': (1, -1),  # shoulder_lift -
-            'a': (2, 1),   # elbow +
-            's': (2, -1),  # elbow -
+            'z': (1, -1),  # shoulder_lift -
+            'x': (1, 1),   # shoulder_lift +
+            'a': (2, -1),  # elbow -
+            's': (2, 1),   # elbow +
             '1': (3, 1),   # wrist +
             '2': (3, -1),  # wrist -
         }
@@ -142,8 +142,8 @@ def main(args=None):
     print('='*50)
     print('Key mappings:')
     print('  Q/W - shoulder_pan (rotate base)')
-    print('  Z/X - shoulder_lift (tilt)')
-    print('  A/S - elbow (bend)')
+    print('  X/Z - shoulder_lift (tilt)')
+    print('  S/A - elbow (bend)')
     print('  1/2 - wrist (rotate)')
     print('')
     print('  SPACE - reset to home')
